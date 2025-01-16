385ec3dfe3237a390a93436ea7dab9f830fa19af
Haron Musyoka <haronmusyoka01@gmail.com>
2025-01-16 17:06:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 const input = document.getElementById("user-input");
-const result = document.getElementById("results-div");
+const result = document.getElementById("re");
 const text = document.getElementById("results-div");
 const check = document.getElementById("check-btn");
 const clear = document.getElementById("clear-btn");
 const phoneNumberRegex = /^(1\s?)?(\(\d{3}\)\s?|\d{3}[-\s]?)\d{3}[-\s]?\d{4}$/;
-
+ 
 
 
 
 check.addEventListener("click", () => {
-
+  const majibu = document.createElement("p");
+majibu.id =  "results-div";
+document.getElementById("jibu").appendChild(majibu)
   if (input.value === "") {
    alert("Please provide a phone number") 
     
   };
   if (phoneNumberRegex.test(input.value)){
-    text.textContent = `Valid US number: ${input.value}`
+    majibu.textContent = `Valid US number: ${input.value}`
 
   } else {
      
-   text.textContent = `Invalid US number: ${input.value}`
+   majibu.textContent = `Invalid US number: ${input.value}`
     
     
   }
-});
-clear.addEventListener("click",()=>{
-  text.textContent = "";
+  clear.addEventListener("click",()=>{
+  majibu.textContent = "";
 })
+});
+
